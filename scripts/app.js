15e14fed68d0e96483d4da5d0a10483cd2804c95
Jose Guaura <jguaura@8amarketing.com>
2020-01-15 18:47:27 -0300

    updates

--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,69 @@
-const searchInput = document.getElementById("search_input");
+//! API 
+const BASE_URL = "https://api.giphy.com/v1/gifs/";
+const API_KEY = "9U7DlV9VJp8CXxdm3HO6rSyXJwWkKyO8";
+
+//? DOM Containers
+const innerContainer = document.getElementById('inner_container');
 const searchAfterContainer = document.getElementById("search_container__after");
 const suggestionsContainer = document.getElementById("suggestions_container");
 const trendsContainer = document.getElementById("trends_container");
-const API_KEY = "9U7DlV9VJp8CXxdm3HO6rSyXJwWkKyO8";
-const BASE_URL = "https://api.giphy.com/v1/gifs/";
-`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=La Rosalia&limit=30`;
+const searchResultsContainer = document.getElementById('results_container')
+const searchResultsOuter = document.getElementById('search_results')
 
-searchInput.addEventListener("click", () => {
+//? DOM Elements
+const searchInput = document.getElementById("search_input");
+const searchbtn = document.getElementById('search_btn')
+const  lens  = document.getElementById('lens')
+const lensInactive = './assets/lupa_inactive.svg'
+const lensActive = './assets/lupa.svg'
+const themeBtn = document.getElementById('theme_btn')
+const themeDropdown = document.getElementById('theme_dropdown')
+const results_text = document.getElementById('results_text')
+
+//? globals
+var query;
+var offset = Math.floor(Math.random() * (0 - 100)) + 100;
+console.log(lens)
+
+searchInput.addEventListener("click", e => {
   searchAfterContainer.classList.toggle("d-flex");
+  searchbtn.classList.toggle('b-pink')
+  let imgSrc = lens.getAttribute('src');
+  imgSrc === "./assets/lupa_inactive.svg" ? lens.setAttribute("src", lensActive) : lens.setAttribute("src", lensInactive)
+
 });
 
-const get = () => {
-  fetch(`${BASE_URL}search?api_key=${API_KEY}&q=random&limit=4`)
+suggestionsContainer.addEventListener("click", e => {
+  if(e.target.classList.contains("btn")) {
+    query = e.target.dataset.title
+    search()
+  }
+})
+
+searchInput.addEventListener('input', e => {
+  query = e.target.value;
+  setTimeout(() => {
+    search()
+  }, 500)
+  // search()
+})
+
+themeBtn.addEventListener('click', () => {
+  themeDropdown.classList.toggle('d-flex')
+})
+
+const getSuggestions = () => {
+  fetch(`${BASE_URL}search?api_key=${API_KEY}&q=${query}&offset=${offset}&limit=4`)
     .then(response => response.json())
     .then(data => {
+      suggestionsContainer.innerHTML = "";
       data.data.map(gif => {
         suggestionsContainer.innerHTML += suggestionsCardTemplate(gif);
       });
     });
 };
 
-get();
+getSuggestions();
 
 const getTrends = () => {
   fetch(`${BASE_URL}trending?api_key=${API_KEY}&limit=12`)
@@ -33,7 +76,23 @@ const getTrends = () => {
       });
     });
 };
+
 getTrends();
+
+const search = () => {
+  fetch(`${BASE_URL}search?api_key=${API_KEY}&q="${query}"&offset=${offset}&limit=12`)
+  .then(response => response.json())
+  .then(data => {
+    innerContainer.innerHTML = "";
+    searchResultsContainer.innerHTML = '';
+    searchResultsOuter.classList.add("d-block")
+    results_text.innerHTML = ` ${query}`
+    data.data.map(gif => {
+      searchResultsContainer.innerHTML += trendsCardTemplate(gif)
+    })
+  })
+}
+
 const suggestionsCardTemplate = gif =>
   `
 <div class="suggestions_card">
@@ -44,7 +103,7 @@ const suggestionsCardTemplate = gif =>
             <div class="card_img" style="background: url(${gif.images.downsized.url}) center / cover no-repeat;
 ">
               <div class="card_btns">
-                <button class="btn">Ver mas...</button>
+                <button class="btn" data-title="${gif.title}">Ver mas...</button>
               </div>
             </div>
           </div>
