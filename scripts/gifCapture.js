43927d004a17f9dc39867e821aae007eb440710d
Jose Guaura <jguaura@8amarketing.com>
2020-02-04 15:56:41 -0300

    updates

--- scripts/gifCapture.js
+++ scripts/gifCapture.js
@@ -5,9 +5,17 @@ const captureTimer = document.getElementById("capture_timer");
 const startBtn = document.getElementById("capture_start");
 const stopBtn = document.getElementById("capture_stop");
 const repeatBtn = document.getElementById("capture_repeat");
+const uploadBtn = document.getElementById('upload_btn')
+const gifPreviewContainer = document.getElementById('gif_preview__container')
+const uploadURL = `https://upload.giphy.com/v1/gifs?api_key=${API_KEY}`
+const previewer = document.querySelector('.previewer')
+const loader = `./assets/loader.png`
 
-var image = document.querySelector("#preview_canvas");6
+var image = document.querySelector("#preview_canvas");
 
+var blobHolder;
+let uploadData = new FormData()
+// innerContainer.innerHTML = ""
 // startBtn.classList.add("d-none");
 // stopBtn.classList.add("d-none");
 
@@ -27,9 +35,11 @@ function stopRecordingCallback() {
   document.querySelector("#capture_timer").innerHTML =
     "Gif recording stopped: " + bytesToSize(recorder.getBlob().size);
   image.src = URL.createObjectURL(recorder.getBlob());
+  blobHolder = recorder.getBlob()
   recorder.camera.stop();
   recorder.destroy();
   recorder = null;
+  
 }
 
 var recorder; // globally accessible
@@ -66,9 +76,36 @@ stopBtn.onclick = function() {
   stopBtn.classList.add("d-none");
   confirmBtnsContainer.classList.remove("d-none");
   recorder.stopRecording(stopRecordingCallback);
+  
 };
 
 repeatBtn.addEventListener("click", e => {
   confirmBtnsContainer.classList.add("d-none");
   startBtn.classList.remove("d-none");
 });
+
+uploadBtn.addEventListener('click', e => {
+  image.src = '';
+  image.setAttribute('src', `${loader}`)
+  uploader()
+})
+
+const uploader = () => {
+  uploadData.append('file', blobHolder, 'asd.gif')
+  fetch(`${uploadURL}`,
+    { method: 'POST',
+      body: uploadData
+    }
+    )
+    .then(response => response.json())
+    .then(data => {
+      const { data: gifData } = data;
+      console.log(gifData.id)
+      captureContainer.classList.add('d-none')
+     
+      gifPreviewContainer.classList.remove('d-none')
+      misGifosSection.classList.remove('d-none')
+      // previewer.setAttribute('src', blobHolder)
+      previewer.src = URL.createObjectURL(blobHolder)
+    })
+  }
